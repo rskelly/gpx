9a80c72e89f16d08489aa2b92107aaa85ac40bae
Rob Skelly <skelly.rob@gmail.com>
2012-03-30 10:06:13 -0700

    First commit.

---
+++ test.py
@@ -0,0 +1,22 @@
+from gpx import *
+
+g = Gpx
+
+for file in ['fells_loop.gpx', 'ashland.gpx']:
+	f = g.load(file)
+
+	print "File: %s" % file
+	
+	print "\n	Tracks: %s" % len(f.tracks)
+	for i in range(0,  len(f.tracks)):
+		print "		Track %s:" % (i+1)
+		for j in range(0, len(f.tracks[i].segments)):
+			print "			Segment %s (%s)" % ((j+1), len(f.tracks[i].segments[j].points))
+
+	print "\n	Waypoints %s" % len(f.waypoints)
+	
+	print "\n	Routes %s" % len(f.routes)
+	for i in range(0, len(f.routes)):
+		print "		Route %s (%s)" % ((i+1), len(f.routes[i].points))
+
+	print "\n------------------------------------------------\n"
